6d657d0fd7d23624c5d19996f89480dc6f9d184e
pharadornb <b6134228@g.sut.ac.th>
2022-06-18 17:54:35 +0700

    Section 3 Exercise 1 - Migration db for connect model other type - Join table and show data - Customer join MembershipTypes

--- asp.net_mvc-webapp/Models/MembershipType.cs
+++ asp.net_mvc-webapp/Models/MembershipType.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp.net_mvc_webapp.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
